51138923ff81b0c1e930df9ced7c03793c9a77b7
Carlos M. Vicente Herrero <cmvicentehe@gmail.com>
2019-06-15 12:16:33 +0200

    Initial Commit

---
+++ ReduxSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReduxSample
+//
+//  Created by Carlos Manuel Vicente Herrero on 15/06/2019.
+//  Copyright © 2019 Carlos Manuel Vicente Herrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
